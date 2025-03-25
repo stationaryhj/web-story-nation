614abe0c15f7b7af5cb6cba6601a100295f22ce8
plumpsqrl9744 <rnwkddn1234@nate.com>
2025-03-25 09:05:46 +0900

    feat: 기획서에 맞게 컨텐츠 추가 #30

--- app/(routes)/my-account/page.tsx
+++ app/(routes)/my-account/page.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 
 import Header from '@/components/common/header'
 import MyAccountView from '@/views/my-account/home'
@@ -6,21 +6,21 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { useAccountStore } from '@/store/useStoreData'
 
-export default function MyAccount() {
-  // const router = useRouter()
-  // const isLogin = useAccountStore(state => state.isLogin)
+export default function MyAccountPage() {
+  const router = useRouter()
+  const isLogin = useAccountStore(state => state.isLogin)
 
-  // useEffect(() => {
-  //   // 로그인 상태가 아니면 로그인 페이지로 리다이렉트
-  //   if (!isLogin) {
-  //     router.push('/login')
-  //   }
-  // }, [isLogin, router])
+  useEffect(() => {
+    // 로그인 상태가 아니면 로그인 페이지로 리다이렉트
+    if (!isLogin) {
+      router.push('/login')
+    }
+  }, [isLogin, router])
 
-  // // 비로그인 상태일 때 임시 로딩 상태 표시
-  // if (!isLogin) {
-  //   return <div className="flex justify-center items-center min-h-screen">로그인 확인 중...</div>
-  // }
+  // 비로그인 상태일 때 임시 로딩 상태 표시
+  if (!isLogin) {
+    return <div className="flex justify-center items-center min-h-screen">로그인 확인 중...</div>
+  }
 
   return (
     <>
