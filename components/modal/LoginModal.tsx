3df8ba4404de80d6a683dfef7c4cb3276adc2d05
plumpsqrl9744 <rnwkddn1234@nate.com>
2025-04-11 17:50:14 +0900

    fix : 간편 회원가입 삭제 #135

--- components/modal/LoginModal.tsx
+++ components/modal/LoginModal.tsx
@@ -276,14 +276,7 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
               </div>
             </button>
           </div>
-          <button
-            onClick={() => setShowSignup(true)}
-            className="mt-4 flex w-full h-12 items-center justify-center rounded-full bg-gray-100 py-1 font-medium text-gray-700 shadow transition-colors hover:bg-gray-200"
-          >
-            <div className="flex items-center justify-center gap-2">
-              <span>간편 회원가입</span>
-            </div>
-          </button>
+
           {/* 
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
@@ -294,7 +287,7 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
             </div>
           </div> */}
 
-          <GuestLoginForm onSubmit={handleGuestLogin} disabled={loading} />
+          {/* <GuestLoginForm onSubmit={handleGuestLogin} disabled={loading} /> */}
 
           {/* 신규 가입 모드일 때만 약관 동의 문구 표시 */}
 
