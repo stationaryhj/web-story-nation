4fe4377099ac00461b1f94d6c25972f57a190f91
강민승 <83339863+minseung-gang@users.noreply.github.com>
2026-01-28 12:39:18 +0900

    chore: 기존 코드 삭제 및 롤백

--- app/(routes)/guest/page.tsx
+++ app/(routes)/guest/page.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import SimpleLogin from "@/views/simple-login/client";
+import React from 'react';
+import SimpleLogin from '@/views/simple-login/client';
 
 type Props = {};
 
 export default function page({}: Props) {
-	return (
-		<>
-			<SimpleLogin />
-		</>
-	);
+  return (
+    <>
+      <SimpleLogin />
+    </>
+  );
 }
