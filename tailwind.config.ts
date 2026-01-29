101c0fe23c7b82931edeea122e1e199bf3038c8d
강민승 <83339863+minseung-gang@users.noreply.github.com>
2026-01-29 19:55:43 +0900

    Feat: max width 관련 브레이크포인트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ const config: Config = {
   ],
   darkMode: 'class',
   theme: {
+    screens: {
+      'max-sm': { max: '639px' },
+      'max-md': { max: '767px' },
+      'max-lg': { max: '1023px' },
+      'max-xl': { max: '1279px' },
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-poppins)', 'sans-serif'],
