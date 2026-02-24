c0d6ba77edc5bb354896c2022ff0a8687b8bda0a
강민승 <83339863+minseung-gang@users.noreply.github.com>
2026-02-24 12:36:39 +0900

    Chore: Edit 컴포넌트 파일경로 수정정

--- app/(routes)/my-characters/edit/[id]/page.tsx
+++ app/(routes)/my-characters/edit/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/common/header';
-import Edit from '@/views/my-characters/edit/ui/Edit';
+import Edit from '@/src/views/my-characters/edit/ui/Edit';
 
 export default function page() {
   return (
